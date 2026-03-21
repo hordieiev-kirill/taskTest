0b2893aa0879a9f318b05608ecd1324f66b92257
hordieiev-kirill <kiril@blocklack.com>
2026-03-21 15:24:33 +0200

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-
+const PORT = process.env.PORT || '4000';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -22,7 +22,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = parseInt(process.env.PORT || '4000', 10);
 server.listen(PORT, () => {
   console.log(`Socket.IO server is running on http://localhost:${PORT}`);
 });
